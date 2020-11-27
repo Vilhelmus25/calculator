c44242a67a2740933f17f5f2e1653dbddecc8791
Vörös Vilmos (Admin) <admin_vorosv@uni-bge.hu>
2020-11-27 13:41:23 +0100

    beviteli mező kész

--- js/calculator.js
+++ js/calculator.js
@@ -1,17 +1,22 @@
 'use strict';
 
-const buttons = Array.from(document.querySelectorAll('[class^="calculator__buttons--"]'));          // az összes gombra egy wildcard kereséssel.   ^= starts with
+const numberButtons = Array.from(document.querySelectorAll('[class^="calculator__buttons--"]'))
+    .map((elements) => elements.firstChild.textContent)
+    .filter(elements => parseInt(elements) == elements)             // itt az a baj, hogy megvizsgálja, stimmel, de stringként megy tovább, nekünk number kell, vagy tömb
+    .map((elements) => parseInt(elements));        //  hát ez bejárja a tömbbe rakott stringeket és integerré alakítja, a forEach az csak kiiratás végett van, majd ki is vesszük
+
+const buttonListener = () => document.querySelectorAll('[class^="calculator__buttons--"]')
+    .forEach(elements => document.addEventListener('click', handleClick));          // az összes gombra egy wildcard kereséssel.   ^= starts with
 //buttons.forEach((elements) => console.log(elements.firstChild));            // bejárja szépen a gombelemeket és kiírja, a firstchild a gombok tartalmát, vagyis a karaktereket adja vissza, ezek stringek
 
-const numberButtons = buttons.map((elements) => elements.firstChild.textContent)
-    .filter(elements => parseInt(elements) == elements)             // itt az a baj, hogy megvizsgálja, stimmel, de stringként megy tovább, nekünk number kell, vagy tömb
-    .forEach((elements) => console.log(typeof elements));        //  hát ez bejárja a tömbbe rakott stringeket és integerré alakítja, a forEach az csak kiiratás végett van, majd ki is vesszük
+const calculatorScreen = document.querySelector('.calculator__screen');         // ha teszünk rá querySelectort, akkor már tudjuk változtatni a textContetn-jét!!!
 
-numberButtons.forEach(elements => elements.addEventListener('click', handleClick));       // eseményfigyelőt teszünk rájuk, kattintásra ('click') és meghívjuk az onScreen metódust
+console.log(numberButtons);
+//numberButtons.forEach(elements => elements.addEventListener('click', handleClick));       // eseményfigyelőt teszünk rájuk, kattintásra ('click') és meghívjuk az onScreen metódust
 
-
-
-function handleClick(item) {
-    (event) => event.target.textContent = item;
-    console.log(item);
+function handleClick(event) {
+    calculatorScreen.textContent = `${calculatorScreen.textContent}${event.target.textContent}`;
+    return calculatorScreen.textContent;
 }
+
+buttonListener();
